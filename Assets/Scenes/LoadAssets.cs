75be787fb569b9b6a59835205608cbc19e2742d4
WEIHUA ZHANG <weihuazhang@WEIHUAdeMacBook-Pro.local>
2024-08-27 10:37:40 +1000

    Instantiated prefabs and set frame rate to 60fps

--- Assets/Scenes/LoadAssets.cs
+++ Assets/Scenes/LoadAssets.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
-{   
-    // Public variable to hold the RedPrefab
+{
+    // Public variable for the red object prefab
     public GameObject redObj;
+
+    // Private variable for the blue object prefab, visible in the Inspector
+    [SerializeField]
+    private GameObject blueObj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Instantiate redObj at position (2, 0, 0) with no rotation
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+
+        // Instantiate blueObj at position (-2, 0, 0) with no rotation
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+
+        // Set the target frame rate to 60 fps
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
-    
 }
